fb30c9126d0ea0dc3865aa24d991663a7c7d5da4
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-08 10:24:55 -0500

    Import font in globalStyle.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -13,6 +13,8 @@ export default class Home extends Component {
       <div>
         <h1>Hi Home!</h1>
         <Image src={require('../assets/us.jpg')} alt="webdevelopers-family"/> 
+        <p>Maecenas facilisis dolor ornare tellus tincidunt condimentum. Quisque massa ante, porttitor at mauris vel, sagittis volutpat neque. Nam iaculis lobortis sapien, a elementum risus luctus id. Nullam accumsan, tortor ac feugiat eleifend, tortor est ultrices justo, sed suscipit odio odio quis est. Donec a mattis ante. Vestibulum volutpat congue mi sit amet condimentum. Nullam ac sapien vel turpis finibus imperdiet et ac ipsum.
+        </p>
       </div>
     );
   }
---
+++ src/theme/globalStyle.js
@@ -0,0 +1,6 @@
+import { injectGlobal } from 'styled-components';
+
+/* eslint-disable */
+injectGlobal`
+   @import url('https://fonts.googleapis.com/css?family=Swanky+and+Moo+Moo');
+`
