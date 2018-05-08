ec1888387e4f85db0dd8096fced03c401e7f4f68
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-08 10:36:02 -0500

    Create container.js file

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -3,4 +3,9 @@ import { injectGlobal } from 'styled-components';
 /* eslint-disable */
 injectGlobal`
    @import url('https://fonts.googleapis.com/css?family=Swanky+and+Moo+Moo');
+
+   body {
+    font-family: 'Swanky and Moo Moo', cursive;
+    font-size: 26px;
+   }
 `
