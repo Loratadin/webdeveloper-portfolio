b96ed65f082f93faec75a264032a192b24a19476
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-09 14:33:41 -0500

    Add projects to routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Home from './containers/Home/Home';
 import App from './containers/App/App';
+import Projects from './containers/Projects/Projects';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 export default (
 <BrowserRouter>
 <div>
 <Route path="/" component={App} />
-<Route path="/" component={Home} />
+<Route exact path="/" component={Home} />
+<Route path="/projects" component={Projects} />
 </div>
 </BrowserRouter>
 );
