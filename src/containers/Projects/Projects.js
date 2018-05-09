908879873d3b26672be6708041014b5fa7d80891
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-09 15:11:12 -0500

    Substitute the project image

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -8,7 +8,7 @@ class Projects extends Component {
      <div>
        <h1>These are the Projects I worked on</h1>
        <Zoomy
-          imageUrl={'https://images.unsplash.com/photo-1487309078313-fad80c3ec1e5?dpr=2&auto=format&fit=crop&w=767&h=1023&q=80&cs=tinysrgb&crop='}
+          imageUrl={require('../../assets/proj-todoapp.jpg')}
           renderThumbnail={({ showImage }) => <button onClick={showImage}>Show Image</button>}
           scale={[1.1, 1.1]}
           imageProps={{
