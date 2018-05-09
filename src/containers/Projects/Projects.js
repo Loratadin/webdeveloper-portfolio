3765f64f30d7903d72f44660303fbf3058d8ca6d
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-09 16:02:48 -0500

    Add styles for projects

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import Zoomy from 'react-zoomy';
 import PropTypes from 'prop-types';
+import {
+   ImageButton
+} from './Projects.style';
+import { Container } from '../../theme/grid';
 
 class Projects extends Component {
  render () {
   return (
-     <div>
+     <Container>
        <h1>These are the Projects I worked on</h1>
        <Zoomy
           imageUrl={require('../../assets/proj-todoapp.jpg')}
           renderThumbnail={({ showImage }) => 
-          <img src={require('../../assets/proj-s-todoapp.jpg')} alt={'todo app'} onClick={showImage}/>
+          <ImageButton onClick={showImage}>
+           <img
+          src={require('../../assets/proj-s-todoapp.jpg')} 
+          alt={'todo app'} 
+          />
+          </ImageButton>
            }
           scale={[1.1, 1.1]}
           imageProps={{
@@ -20,7 +29,7 @@ class Projects extends Component {
     }
   }}
 />
-     </div>
+     </Container>
   );
  }
 }
