5aad5dcf46f2ecbdb289feb89e4589f86804c9ed
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-09 14:24:43 -0500

    Set up Projects.js file

---
+++ src/containers/Projects/Projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+ render () {
+  return (
+     <div>
+       <h1>These are the Projects I worked on</h1>
+     </div>
+  );
+ }
+}
+
+export default Projects;
