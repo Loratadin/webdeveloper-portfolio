d6f63e733fc200fdbbb265d0be69d583bcfdbd5e
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-09 19:28:24 -0500

    Set bg styles in App.style.js

--- src/containers/App/App.style.js
+++ src/containers/App/App.style.js
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+
+export const Background = styled.div`
+ position: fixed;
+ left: 10vw;
+ width: 80vw;
+ top: 10vh;
+ height: 80vh;
+ background-image: url(${require('../assets/us-bg.png')});
+ background-color: rgba(238, 251, 159, 0.75);
+`;
