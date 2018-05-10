2f9596e41b873afa2f4b5c730b8d22264553574e
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-10 15:40:52 -0500

    Upload background picture for home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -29,6 +29,24 @@ export default class Home extends Component {
              </RevealP>
         }
         </WhenInView>
+        <WhenInView>
+          {( { isInView } ) => 
+             <RevealP hide={!isInView}>Thank you for visiting this website and being interested to know who I am. My name is Tania, and Shulga is my last name. I am from Belarus, therefore, my name is spelled like Tatsiana Shulha in my driving license.
+             </RevealP>
+        }
+        </WhenInView>
+        <WhenInView>
+          {( { isInView } ) => 
+             <RevealP hide={!isInView}>Thank you for visiting this website and being interested to know who I am. My name is Tania, and Shulga is my last name. I am from Belarus, therefore, my name is spelled like Tatsiana Shulha in my driving license.
+             </RevealP>
+        }
+        </WhenInView>
+        <WhenInView>
+          {( { isInView } ) => 
+             <RevealP hide={!isInView}>Thank you for visiting this website and being interested to know who I am. My name is Tania, and Shulga is my last name. I am from Belarus, therefore, my name is spelled like Tatsiana Shulha in my driving license.
+             </RevealP>
+        }
+        </WhenInView>
       </Container>
     );
   }
