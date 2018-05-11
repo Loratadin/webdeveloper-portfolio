36c0855d36c140946cf3d038729f39de1ca77e2d
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-11 18:40:33 -0500

    Add burger menu style file

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 class BurgerMenu extends Component {
  render() {
   return (
-   <div>
-    
-   </div>
+    <div id="burger">
+    <div id="burger-hover"></div>
+    <div class="burger-line-wrap-0 burger-line-wrap"></div>
+    <div class="burger-line-wrap-1 burger-line-wrap"></div>
+    <div class="burger-line-wrap-2 burger-line-wrap"></div>
+    <div class="burger-close-wrap-0 burger-close-wrap"></div>
+    <div class="burger-close-wrap-1 burger-close-wrap"></div>
+    <div class="burger-border-wrap"></div>
+    <div class="burger-mask"></div>
+    </div>
   );
  }
 }
---
+++ src/components/BurgerMenu/BurgerMenu.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Burger = styled.div`
+ position: fixed;
+ z-index: 3;
+ overflow: hidden;
+`;
