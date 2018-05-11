bf2763bade0414a745435bfc4b9d78c2b1a12c3e
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-11 11:40:50 -0500

    Set up NavigationBar file

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NavigationBar extends Component {
+ render() {
+  return (
+   <div>
+    <div>Home</div>
+    <div>Projects</div>
+    <div>About me</div>
+   </div>
+  );
+ }
+}
+
+export default NavigationBar;
