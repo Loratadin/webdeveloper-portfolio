183c90dbcb796970d38f4324e850d3e482a2bf5c
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-13 11:32:12 -0500

    Add AboutMe page to routers

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './containers/Home/Home';
 import App from './containers/App/App';
 import Projects from './containers/Projects/Projects';
+import AboutMe from './containers/AboutMe/AboutMe';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 export default (
@@ -10,6 +11,7 @@ export default (
 <Route path="/" component={App} />
 <Route exact path="/" component={Home} />
 <Route path="/projects" component={Projects} />
+<Route path="/aboutme" component={AboutMe} />
 </div>
 </BrowserRouter>
 );
