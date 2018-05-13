28d3969fd348b1381a47ccf41ca05341572f491f
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-13 11:38:04 -0500

    Add background for AboutMe page

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Container } from '../../theme/grid';
+import { Background } from '../App/App.style';
 
 class AboutMe extends Component {
  render(){
   return (
-    <div>
+   <Container>
+   <Background><img src={require('../../assets/doggy.png')} alt={'Doggy'}/></Background>
       <h1>About me</h1>
-    </div>
+    </Container>
   );
  }
 }
