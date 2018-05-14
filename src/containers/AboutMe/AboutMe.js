7a90d10794c7b259d4f010613f2a76b2cef45511
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-14 11:14:49 -0500

    Add first question to about me page

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -8,6 +8,9 @@ class AboutMe extends Component {
    <Container>
    <Background><img src={require('../../assets/doggy.png')} alt={'Doggy'}/></Background>
       <h1>About me</h1>
+      <h2>Tell me something to inspire me today</h2>
+      <h2>Ok! All you need to know is that everything is possible. We live in a wonderful world that is full of beauty, charm, and adventure. There is no end to the adventures we can have if only we seek them with our eyes open.
+      </h2>
     </Container>
   );
  }
