cbbadbc4486633f94cb53247f087a45be7ddf262
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-14 20:48:11 -0500

    Style the play button

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -3,8 +3,8 @@ import { Container } from '../../theme/grid';
 import { Background } from '../App/App.style';
 import ReactPlayer from 'react-player';
 import {H2} from '../../theme/types';
-import { VideoContainer } from './AboutMe.style';
-import PlayButton from '../../components/PlayButton/PlayButton';
+import { VideoContainer, StyledPlayButton } from './AboutMe.style';
+
 
 class AboutMe extends Component {
  render(){
@@ -16,9 +16,10 @@ class AboutMe extends Component {
       <H2 align="center">Ok! All you need to know is that everything is possible. We live in a wonderful world that is full of beauty, charm, and adventure. There is no end to the adventures we can have if only we seek them with our eyes open.
       </H2>
       <VideoContainer>
-       <PlayButton/>
+       <StyledPlayButton/>
         <ReactPlayer 
          width="100%"
+         height="auto"
          controls
          url={require('../../assets/video.mp4')}/>
       </VideoContainer>
--- src/containers/AboutMe/AboutMe.style.js
+++ src/containers/AboutMe/AboutMe.style.js
@@ -1,14 +1,23 @@
 import styled from 'styled-components';
 import media from '../../theme/media';
 import { Relative } from '../../theme/grid';
+import PlayButton from '../../components/PlayButton/PlayButton';
 
 export const VideoContainer = styled(Relative)`
   width: 90%;
   margin: 0 auto;
+  cursor: pointer;
   ${media.desktop`
   width: 80%;
    `}
   ${media.monitor`
   width: 70%;
    `}
+`;
+
+export const StyledPlayButton = styled(PlayButton)`
+   position: absolute;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
 `;
