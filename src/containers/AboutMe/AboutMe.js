9de9aea7ad4999aeee2351ba10d3451effca2514
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-15 10:57:22 -0500

    Add content to About me page

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -24,19 +24,22 @@ class AboutMe extends Component {
    <Container>
    <Background><img src={require('../../assets/doggy.png')} alt={'Doggy'}/></Background>
       <h1>About me</h1>
-      <H2 align="right">Tell me something to inspire me today</H2>
-      <ShowMe onClick={() => this.operation()}></ShowMe>
+      <H2 align="left">I wrote the questions you might want to ask me. Let's start! Hit the lime to get the answers.</H2>
+      <H2 align="left">Tell me something to inspire me today</H2>
+      <ShowMe onClick={() => this.operation()}><img src={require('../../assets/lime.png')} alt={'lime'}/></ShowMe>
       { this.state.showMe ? 
+      <div>
           <H2 align="center">Ok! All you need to know is that everything is possible. We live in a wonderful world that is full of beauty, charm, and adventure. There is no end to the adventures we can have if only we seek them with our eyes open.
           </H2>
+          <VideoContainer>
+          <StyledPlayButton/>
+            <ReactPlayer 
+            width="100%"
+            height="auto"
+            url={require('../../assets/video.mp4')}/>
+         </VideoContainer>
+         </div>
     : null}
-           <VideoContainer>
-           <StyledPlayButton/>
-             <ReactPlayer 
-             width="100%"
-             height="auto"
-             url={require('../../assets/video.mp4')}/>
-          </VideoContainer>
     </Container>
   );
  }
--- src/containers/AboutMe/AboutMe.style.js
+++ src/containers/AboutMe/AboutMe.style.js
@@ -25,5 +25,20 @@ export const StyledPlayButton = styled(PlayButton)`
 
 export const ShowMe = styled.button`
   cursor: pointer;
-  background-color: green;
+  animation: spin infinite 5s linear;
+  right: 0;
+  top:0;
+  width: 60px;
+  height: 60px;
+  padding: 0;
+  margin: 0;
+  border: none;
+  outline: inherit;
+  & > img {
+    width: 100%;
+  }
+  @keyframes spin {
+    from {transform:rotate(0deg);}
+    to {transform:rotate(360deg);}
+}
 `;
