0c17ad7f6d696fdc76a4e04dc0741cc590fc5193
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-15 11:06:29 -0500

    Create ShowMe component

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -40,6 +40,22 @@ class AboutMe extends Component {
          </VideoContainer>
          </div>
     : null}
+
+    <H2 align="left">Tell me something to inspire me today</H2>
+      <ShowMe onClick={() => this.operation()}><img src={require('../../assets/lime.png')} alt={'lime'}/></ShowMe>
+      { this.state.showMe ? 
+      <div>
+          <H2 align="center">Ok! All you need to know is that everything is possible. We live in a wonderful world that is full of beauty, charm, and adventure. There is no end to the adventures we can have if only we seek them with our eyes open.
+          </H2>
+          <VideoContainer>
+          <StyledPlayButton/>
+            <ReactPlayer 
+            width="100%"
+            height="auto"
+            url={require('../../assets/video.mp4')}/>
+         </VideoContainer>
+         </div>
+    : null}
     </Container>
   );
  }
