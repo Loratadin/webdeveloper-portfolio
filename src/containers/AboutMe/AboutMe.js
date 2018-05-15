bf828306f1342324aafcbe9d89666d18a4a91c36
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-15 10:13:44 -0500

    Create ShowMe button on About me page

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -6,17 +6,33 @@ import {H2} from '../../theme/types';
 import { VideoContainer, StyledPlayButton } from './AboutMe.style';
 
 class AboutMe extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {
+        showMe: false
+    };
+  }
+  
+  operation() {
+    this.setState({
+      showMe: !this.state.showMe
+    })
+  }
  render(){
   return (
    <Container>
    <Background><img src={require('../../assets/doggy.png')} alt={'Doggy'}/></Background>
       <h1>About me</h1>
       <H2 align="right">Tell me something to inspire me today</H2>
-      <H2 align="center">Ok! All you need to know is that everything is possible. We live in a wonderful world that is full of beauty, charm, and adventure. There is no end to the adventures we can have if only we seek them with our eyes open.
-      </H2>
+      <button onClick={() => this.operation()}></button>
+      { this.state.showMe ? 
+          <H2 align="center">Ok! All you need to know is that everything is possible. We live in a wonderful world that is full of beauty, charm, and adventure. There is no end to the adventures we can have if only we seek them with our eyes open.
+          </H2>
+    : null}
            <VideoContainer>
            <StyledPlayButton/>
-            <ReactPlayer 
+             <ReactPlayer 
              width="100%"
              height="auto"
              url={require('../../assets/video.mp4')}/>
