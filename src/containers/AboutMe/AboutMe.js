dcc39eca303ce1fd304f12cfa25ce676978b11ea
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-14 19:05:30 -0500

    Add H2 styled component

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Container } from '../../theme/grid';
 import { Background } from '../App/App.style';
 import ReactPlayer from 'react-player';
+import {H2} from '../../theme/types';
 
 class AboutMe extends Component {
  render(){
@@ -9,7 +10,7 @@ class AboutMe extends Component {
    <Container>
    <Background><img src={require('../../assets/doggy.png')} alt={'Doggy'}/></Background>
       <h1>About me</h1>
-      <h2>Tell me something to inspire me today</h2>
+      <H2 align="right">Tell me something to inspire me today</H2>
       <h2>Ok! All you need to know is that everything is possible. We live in a wonderful world that is full of beauty, charm, and adventure. There is no end to the adventures we can have if only we seek them with our eyes open.
       </h2>
       <ReactPlayer 
