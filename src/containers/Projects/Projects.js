9ea2b1991c8f3c35ecbb9e8f69dfdce394af4585
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-05-20 20:38:03 -0500

    Add GitHub and AWS links

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -42,10 +42,14 @@ class Projects extends Component {
            <h3>01</h3>
          </Number>
          </Relative>
-         <h2>React based <Link href="https://github.com/Loratadin/react-todo-app" target="_blank">To do App</Link></h2>
+         <h2>React based <Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/react-todo-app/" target="_blank">To do App</Link></h2>
          <p>
          Implemented a fast performing application which allows creating, editing and deleting tasks. Created reusable components to make the application scalable. Implemented a validation, so that the app shows error messages if a task already exists or a user is trying to save an empty task.
-         <GitLink href="https://github.com/Loratadin/react-todo-app" target="_blank"><img src={require('../../assets/github-dark.png')} alt={'GitHub'}/></GitLink>
+         <GitLink 
+         href="https://github.com/Loratadin/react-todo-app" 
+         target="_blank"><img src={require('../../assets/github-dark.png')} 
+         alt={'GitHub'}/>
+         </GitLink>
          </p>
         </ProjectBio>
        </ProjectWrapper>
@@ -58,9 +62,14 @@ class Projects extends Component {
            <h3>02</h3>
          </Number>
          </Relative>
-       <h2><Link href="https://github.com/Loratadin/montessori" target="_blank">Montessori School</Link> Concept</h2>
+       <h2><Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/montessori/" target="_blank">Montessori School</Link> Concept</h2>
          <p>
          Implemented a homepage with a responsive design. Used Vanila JavaScript to create such features as real time clock, search form, theme color change controls, and sign up form. Created a flex panel photo gallery with delightful animation. 
+         <GitLink 
+         href="https://github.com/Loratadin/montessori" 
+         target="_blank"><img src={require('../../assets/github-dark.png')} 
+         alt={'GitHub'}/>
+         </GitLink>
          </p>
         </ProjectBio>
         <Zoomy
@@ -108,9 +117,14 @@ class Projects extends Component {
            <h3>03</h3>
          </Number>
          </Relative>
-         <h2><Link href="https://github.com/Loratadin/coffeeshop" target="_blank">Coffeeshop</Link> Concept</h2>
+         <h2><Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/coffeeshop" target="_blank">Coffeeshop</Link> Concept</h2>
          <p>
          Delivered high-quality homepage for a coffee shop concept. Created delightful design with parallax effect in CSS, fully responsive for any device. Implemented a full screen slider, and an animated skewed section with Vanilla JavaScript. Extended default JavaScript functionality by using jQuery for a hamburger menu.
+         <GitLink 
+         href="https://github.com/Loratadin/coffeeshop" 
+         target="_blank"><img src={require('../../assets/github-dark.png')} 
+         alt={'GitHub'}/>
+         </GitLink>
          </p>
         </ProjectBio>
        </ProjectWrapper>
@@ -122,9 +136,14 @@ class Projects extends Component {
            <h3>04</h3>
          </Number>
          </Relative>
-       <h2><Link href="https://github.com/Loratadin/Dogsperience" target="_blank">Dogsperience</Link> Concept</h2>
+       <h2><Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/dogsperience/" target="_blank">Dogsperience</Link> Concept</h2>
          <p>
          Implemented a static homepage for an online pet store concept. Used HTML, CSS and jQuery for the slider.
+         <GitLink 
+         href="https://github.com/Loratadin/Dogsperience" 
+         target="_blank"><img src={require('../../assets/github-dark.png')} 
+         alt={'GitHub'}/>
+         </GitLink>
          </p>
         </ProjectBio>
         <Zoomy
@@ -172,9 +191,14 @@ class Projects extends Component {
            <h3>05</h3>
          </Number>
          </Relative>
-         <h2><Link href="https://github.com/Loratadin/md-portfolio" target="_blank">Portfolio</Link> Concept for a Doctor of Medicine</h2>
+         <h2><Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/md-portfolio/" target="_blank">Portfolio</Link> Concept for a Doctor of Medicine</h2>
          <p>
          Created design and implemented a fully responsive personal portfolio concept for a medical professional. Integrated JavaScript for navigation menu. Wrote modular, mobile-first CSS with help of SASS preprocessor. Used Parcel as a module bundler. Created contact form and animated gallery.  
+         <GitLink 
+         href="https://github.com/Loratadin/md-portfolio" 
+         target="_blank"><img src={require('../../assets/github-dark.png')} 
+         alt={'GitHub'}/>
+         </GitLink>
          </p>
         </ProjectBio>
        </ProjectWrapper>
