0ca832d7de8276322eefba4da96cfe1b6498c626
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-07 11:22:05 -0500

    Created Coffeeshop component

--- src/components/Montessori.js
+++ src/components/Montessori.js
@@ -22,7 +22,7 @@ class Montessori extends Component {
         </Relative>
         <h2><Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/montessori/" target="_blank">Montessori School</Link> Concept</h2>
         <p>
-         Implemented a fast performing application which allows creating, editing and deleting tasks. Created reusable components to make the application scalable. Implemented a validation, so that the app shows error messages if a task already exists or a user is trying to save an empty task.
+        Implemented a homepage with a responsive design. Used Vanila JavaScript to create such features as real time clock, search form, theme color change controls, and sign up form. Created a flex panel photo gallery with delightful animation.
         <GitLink 
           href="https://github.com/Loratadin/montessori" 
           target="_blank"><img src={require('../assets/github-dark.png')} 
--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -13,6 +13,7 @@ import { Background } from '../App/App.style';
 import WeatherApp from '../../components/WeatherApp';
 import ToDoApp from '../../components/ToDoApp';
 import Montessori from '../../components/Montessori';
+import Coffeeshop from '../../components/Coffeeshop';
 
 class Projects extends Component {
  render () {
@@ -23,43 +24,7 @@ class Projects extends Component {
  
         <ToDoApp/>
         <Montessori/>
-
-       <ProjectWrapper>
-       <Zoomy
-          imageUrl={require('../../assets/proj-coffeeshop.jpg')}
-          renderThumbnail={({ showImage }) => 
-          <ImageButton onClick={showImage}>
-           <img
-          src={require('../../assets/proj-s-coffeeshop.jpg')} 
-          alt={'coffeeshop'} 
-          />
-          </ImageButton>
-           }
-          scale={[1.1, 1.1]}
-          imageProps={{
-          style: {
-          width: '100vw',
-          height: 'auto'
-           }
-          }}
-        />
-        <ProjectBio>
-        <Relative>
-         <Number>
-           <h3>03</h3>
-         </Number>
-         </Relative>
-         <h2><Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/coffeeshop" target="_blank">Coffeeshop</Link> Concept</h2>
-         <p>
-         Delivered high-quality homepage for a coffee shop concept. Created delightful design with parallax effect in CSS, fully responsive for any device. Implemented a full screen slider, and an animated skewed section with Vanilla JavaScript. Extended default JavaScript functionality by using jQuery for a hamburger menu.
-         <GitLink 
-         href="https://github.com/Loratadin/coffeeshop" 
-         target="_blank"><img src={require('../../assets/github-dark.png')} 
-         alt={'GitHub'}/>
-         </GitLink>
-         </p>
-        </ProjectBio>
-       </ProjectWrapper>
+        <Coffeeshop/>
 
        <ProjectWrapper>
        <ProjectBio>
