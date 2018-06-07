63723f11ccf9a2a007e14f196a222be4f4e88596
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-07 11:36:59 -0500

    Add Mdportfolio component

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
-import Zoomy from 'react-zoomy';
-import {
-   ImageButton,
-   ProjectBio,
-   ProjectWrapper,
-   Number,
-   GitLink
-} from './Projects.style';
-import { Container, Relative } from '../../theme/grid';
-import { Link } from '../../theme/types';
+import { Container } from '../../theme/grid';
 import { Background } from '../App/App.style';
-import WeatherApp from '../../components/WeatherApp';
 import ToDoApp from '../../components/ToDoApp';
 import Montessori from '../../components/Montessori';
 import Coffeeshop from '../../components/Coffeeshop';
 import Dogsperience from '../../components/Dogsperience';
+import Mdportfolio from '../../components/Mdportfolio';
+import WeatherApp from '../../components/WeatherApp';
 
 class Projects extends Component {
  render () {
@@ -27,48 +19,10 @@ class Projects extends Component {
         <Montessori/>
         <Coffeeshop/>
         <Dogsperience/>
-
-       <ProjectWrapper>
-       <Zoomy
-          imageUrl={require('../../assets/proj-mdportfolio.jpg')}
-          renderThumbnail={({ showImage }) => 
-          <ImageButton onClick={showImage}>
-           <img
-          src={require('../../assets/proj-s-mdportfolio.jpg')} 
-          alt={'professional portfolio'} 
-          />
-          </ImageButton>
-           }
-          scale={[1.1, 1.1]}
-          imageProps={{
-          style: {
-          width: '100vw',
-          height: 'auto'
-           }
-          }}
-        />
-        <ProjectBio>
-        <Relative>
-         <Number>
-           <h3>05</h3>
-         </Number>
-         </Relative>
-         <h2><Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/md-portfolio/" target="_blank">Portfolio</Link> Concept for a Doctor of Medicine</h2>
-         <p>
-         Created design and implemented a fully responsive personal portfolio concept for a medical professional. Integrated JavaScript for navigation menu. Wrote modular, mobile-first CSS with help of SASS preprocessor. Used Parcel as a module bundler. Created contact form and animated gallery.  
-         <GitLink 
-         href="https://github.com/Loratadin/md-portfolio" 
-         target="_blank"><img src={require('../../assets/github-dark.png')} 
-         alt={'GitHub'}/>
-         </GitLink>
-         </p>
-        </ProjectBio>
-       </ProjectWrapper>
-
-       <WeatherApp/>
-       
-        
-     </Container>
+        <Mdportfolio/>
+        <WeatherApp/>
+   
+    </Container>
   );
  }
 }
