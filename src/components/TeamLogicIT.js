287054fb3d61a7a7caeb3e89e9deedb472b1725e
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-22 14:03:49 -0500

    Create TeamLogicIT component

---
+++ src/components/TeamLogicIT.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Zoomy from 'react-zoomy';
+import {
+   ImageButton,
+   ProjectBio,
+   ProjectWrapper,
+   Number,
+   GitLink
+} from '../containers/Projects/Projects.style';
+import { Relative } from '../theme/grid';
+import { Link } from '../theme/types';
+
