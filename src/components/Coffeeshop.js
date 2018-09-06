628649b0d52cec623b55f879cc82a359f6606f4e
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-09-06 18:30:24 -0500

    Edit Zoomy in Coffeshop Component

--- src/components/Coffeeshop.js
+++ src/components/Coffeeshop.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Zoomy from 'react-zoomy';
 import {
    ImageButton,
    ProjectBio,
    ProjectWrapper,
    Number,
-   GitLink
+   GitLink,
+   Zoomy
 } from '../containers/Projects/Projects.style';
 import { Relative } from '../theme/grid';
 import { Link } from '../theme/types';
@@ -14,24 +14,13 @@ class Coffeeshop extends Component {
  render () {
   return (
     <ProjectWrapper>
-      <Zoomy
-        imageUrl={require('../assets/proj-coffeeshop.jpg')}
-        renderThumbnail={({ showImage }) => 
-          <ImageButton onClick={showImage}>
+      <Zoomy href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/coffeeshop/" target="_blank">
             <img
               src={require('../assets/proj-s-coffeeshop.jpg')} 
               alt={'coffeeshop concept'} 
             />
-          </ImageButton>
         }
-        scale={[1.1, 1.1]}
-        imageProps={{
-          style: {
-          width: '100vw',
-          height: 'auto'
-          }
-        }}
-      />
+      </Zoomy>
       <ProjectBio>
         <Relative>
           <Number>
