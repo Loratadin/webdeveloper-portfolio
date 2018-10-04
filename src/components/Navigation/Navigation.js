7c04e6754f940f9448549c82ff7cf9829520a6ce
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-04 12:49:43 -0500

    Delete unused component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { NavigationContainer, NavItem, NavLink } from './Navigation.style';
-import { Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navigation.css';
 
 const DecorationNone = {
