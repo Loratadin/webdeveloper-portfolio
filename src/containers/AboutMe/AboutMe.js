80ee5605c164eb12ef682d7e276b1d21e4e97f77
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-25 12:01:02 -0500

    import animation on scroll to about me page

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Container } from '../../theme/grid';
 import { Background } from '../App/App.style';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import VideoInspire from '../../components/VideoInspire/VideoInspire';
 import AudioMusic from '../../components/AudioMusic/AudioMusic';
 import VideoTalk from '../../components/VideoTalk/VideoTalk';
@@ -11,7 +13,7 @@ class AboutMe extends Component {
    <Container>
    <Background><img src={require('../../assets/doggy.png')} alt={'Doggy'}/></Background>
       <h1>About me</h1>
-      <VideoInspire/>
+      <VideoInspire data-aos="fade-up"/>
       <AudioMusic/>
       <VideoTalk/>
     </Container>
