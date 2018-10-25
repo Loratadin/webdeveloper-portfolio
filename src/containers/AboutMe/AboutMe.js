a07d4afaf90d88fdc1cbe8f80aa13c8f382ad193
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-25 15:14:57 -0500

    initiate aos in constructor

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { Container } from '../../theme/grid';
 import { Background } from '../App/App.style';
-import AOS from 'aos';
+import aos from 'aos';
 import 'aos/dist/aos.css';
 import VideoInspire from '../../components/VideoInspire/VideoInspire';
 import AudioMusic from '../../components/AudioMusic/AudioMusic';
 import VideoTalk from '../../components/VideoTalk/VideoTalk';
 
 class AboutMe extends Component {
+  constructor() {
+    super()
+      aos.init({
+        once: true
+      })
+  }
  render(){
   return (
    <Container>
