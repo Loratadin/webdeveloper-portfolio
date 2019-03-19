e0c635fd709daa2501a06d9713e8ea201ebc7076
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-03-18 21:33:17 -0500

    format the code

--- src/components/ToDoApp.js
+++ src/components/ToDoApp.js
@@ -1,43 +1,43 @@
 import React, { Component } from 'react';
 import {
-   ProjectBio,
-   ProjectWrapper,
-   Number,
-   GitLink,
-   Zoomy
+  ProjectBio,
+  ProjectWrapper,
+  Number,
+  GitLink,
+  Zoomy
 } from '../containers/Projects/Projects.style';
 import { Relative } from '../theme/grid';
 import { Link } from '../theme/types';
 
 class ToDoApp extends Component {
- render () {
-  return (
-    <ProjectWrapper>
-      <Zoomy href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/react-todo-app/" target="_blank">
-            <img
-              src={require('../assets/proj-s-todoapp.jpg')} 
-              alt={'react todo app'} 
-            />
-      </Zoomy>
-      <ProjectBio>
-        <Relative>
-          <Number>
-            <h3>07</h3>
-          </Number>
-        </Relative>
-        <h2>React based <Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/react-todo-app/" target="_blank">To do App</Link></h2>
-        <p>
-         Implemented a fast performing application which allows creating, editing and deleting tasks. Created reusable components to make the application scalable. Implemented a validation, so that the app shows error messages if a task already exists or a user is trying to save an empty task.
-        <GitLink 
-          href="https://github.com/Loratadin/react-todo-app" 
-          target="_blank"><img src={require('../assets/github-dark.png')} 
-          alt={'GitHub'}/>
-        </GitLink>
-        </p>
-      </ProjectBio>
-    </ProjectWrapper>
-  );
- }
+  render() {
+    return (
+      <ProjectWrapper>
+        <Zoomy href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/react-todo-app/" target="_blank">
+          <img
+            src={require('../assets/proj-s-todoapp.jpg')}
+            alt={'react todo app'}
+          />
+        </Zoomy>
+        <ProjectBio>
+          <Relative>
+            <Number>
+              <h3>07</h3>
+            </Number>
+          </Relative>
+          <h2>React based <Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/react-todo-app/" target="_blank">To do App</Link></h2>
+          <p>
+            Implemented a fast performing application which allows creating, editing and deleting tasks. Created reusable components to make the application scalable. Implemented a validation, so that the app shows error messages if a task already exists or a user is trying to save an empty task.
+        <GitLink
+              href="https://github.com/Loratadin/react-todo-app"
+              target="_blank"><img src={require('../assets/github-dark.png')}
+                alt={'GitHub'} />
+            </GitLink>
+          </p>
+        </ProjectBio>
+      </ProjectWrapper>
+    );
+  }
 }
 
 export default ToDoApp;
--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,43 +1,43 @@
 import React, { Component } from 'react';
 import {
-   ProjectBio,
-   ProjectWrapper,
-   Number,
-   GitLink,
-   Zoomy
+  ProjectBio,
+  ProjectWrapper,
+  Number,
+  GitLink,
+  Zoomy
 } from '../containers/Projects/Projects.style';
 import { Relative } from '../theme/grid';
 import { Link } from '../theme/types';
 
 class WeatherApp extends Component {
- render () {
-  return (
-   <ProjectWrapper>
-    <ProjectBio>
-      <Relative>
-        <Number>
-          <h3>06</h3>
-        </Number>
-      </Relative>
-      <h2><Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/weather-app/" target="_blank">Weather App</Link> on React</h2>
-      <p>
-        Created React based weather application that fetches data from a weather API. Applied styled components to make the styles more maintainable. Implemented a validation, so that the app doesn’t brake but shows error messages. 
-        <GitLink 
-          href="https://github.com/Loratadin/weather-app" 
-          target="_blank"><img src={require('../assets/github-dark.png')} 
-          alt={'GitHub'}/>
-        </GitLink>
-      </p>
-    </ProjectBio>
-    <Zoomy href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/weather-app/" target="_blank">
-      <img
-        src={require('../assets/proj-s-weather.jpg')} 
-        alt={'weather app on React'} 
-      />
-    </Zoomy>
-   </ProjectWrapper>
-  );
- }
+  render() {
+    return (
+      <ProjectWrapper>
+        <ProjectBio>
+          <Relative>
+            <Number>
+              <h3>06</h3>
+            </Number>
+          </Relative>
+          <h2><Link href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/weather-app/" target="_blank">Weather App</Link> on React</h2>
+          <p>
+            Created React based weather application that fetches data from a weather API. Applied styled components to make the styles more maintainable. Implemented a validation, so that the app doesn’t brake but shows error messages.
+        <GitLink
+              href="https://github.com/Loratadin/weather-app"
+              target="_blank"><img src={require('../assets/github-dark.png')}
+                alt={'GitHub'} />
+            </GitLink>
+          </p>
+        </ProjectBio>
+        <Zoomy href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/weather-app/" target="_blank">
+          <img
+            src={require('../assets/proj-s-weather.jpg')}
+            alt={'weather app on React'}
+          />
+        </Zoomy>
+      </ProjectWrapper>
+    );
+  }
 }
 
 export default WeatherApp;
