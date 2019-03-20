1cd96a09d67f913388281289a0c1c278b2d4fa1d
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-03-20 15:22:20 -0500

    install SASS

--- src/containers/Home/HomeNew.js
+++ src/containers/Home/HomeNew.js
@@ -5,7 +5,8 @@ export default class Home extends Component {
     render() {
         return (
             <div className="home-wrap">
-                <div className="home"></div>
+                <div className="home-0"></div>
+                <div className="home-1"></div>
             </div>
         );
     }
