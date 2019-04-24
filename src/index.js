058e7502dbd0281612a669a3bc9a8999f7fa3252
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-04-24 04:35:41 -0500

    delete unnecessary dependences

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom';
 import routes from './routes';
 import './theme/globalStyle';
-import './style.scss';
 
 
 ReactDOM.render(
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import App from './containers/App/App';
 import Projects from './containers/Projects/Projects';
 import AboutMe from './containers/AboutMe/AboutMe';
 import { BrowserRouter, Route } from 'react-router-dom';
-import './style.scss';
 
 export default (
     <BrowserRouter>
