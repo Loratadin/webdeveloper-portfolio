f364bd976e7ea000edca5d444b14c69a3bac9533
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-04-30 10:38:04 -0500

    restyle social links with CSS Grid

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,19 +6,19 @@ export default class Home extends Component {
     return (
       <div className="home__wrapper">
         <div className="home-socials__wrapper">
-          <div className="home__socials">
+          <div className="home__socials-item home__socials-item-01">
             <a className="home-socials__link" href="https://github.com/Loratadin" target="_blank" rel="noopener noreferrer" >
               <img className="home-socials__image" src={require('../../assets/github.png')} alt="github" />
             </a>
           </div>
-          <div className="home__socials">
+          <div className="home__socials-item home__socials-item-02">
             <a className="home-socials__link" href="mailto:taniashulha@gmail.com">
-              <img className="home-socials__image" src={require('../../assets/email.png')} alt={'email'} />
+              <img className="home-socials__image" src={require('../../assets/email.png')} alt="email" />
             </a>
           </div>
-          <div className="home__socials">
+          <div className="home__socials-item home__socials-item-03">
             <a className="home-socials__link" href="https://www.linkedin.com/in/tatsiana-shulha-450413b8/" target="_blank" rel="noopener noreferrer" >
-              <img className="home-socials__image" src={require('../../assets/linkedin.png')} alt={'linkedin'} />
+              <img className="home-socials__image" src={require('../../assets/linkedin.png')} alt="linkedin" />
             </a>
           </div>
         </div>
