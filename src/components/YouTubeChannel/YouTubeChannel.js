3e94644f4b03994abdb397fd7a44ca8ce3f8593e
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-05-17 16:14:18 -0500

    add question-answer for YouTubeChannel component

--- src/components/YouTubeChannel/YouTubeChannel.js
+++ src/components/YouTubeChannel/YouTubeChannel.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { H2 } from '../../theme/types';
 import "./YouTubeChannel.css";
 
 export default class YouTubeChannel extends Component {
     render() {
         return (
-            <div className="youtube__wrap">YouTube</div>
+            <div className="youtube__wrap">
+                <H2 align="right">Why do you publish videos on YouTube?</H2>
+                <H2 align="center">There are topics that draw my attention because I faced challenges and after a long struggle found out "how it works". For some questions I couldn't find an answer on YouTube, so there was a space to create one by myself.
+                </H2>
+            </div>
         )
     }
 }
