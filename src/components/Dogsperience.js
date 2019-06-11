eecc3ea802e64fc93cb4dcd83de5f19815793ac9
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-06-11 14:39:49 -0500

    update project image

--- src/components/Dogsperience.js
+++ src/components/Dogsperience.js
@@ -23,7 +23,7 @@ class Dogsperience extends Component {
 					<p>
 						Implemented a React based application that asynchronously gets dog breed names and images from an API. Applied mobile first responsive styles.
         				<GitLink
-							href="https://github.com/Loratadin/dogsperience"
+							href="https://github.com/Loratadin/react-dog-app-tutorial"
 							target="_blank"
 						>
 							<img src={require('../assets/github-dark.png')} alt={'GitHub'} />
@@ -32,7 +32,7 @@ class Dogsperience extends Component {
 				</ProjectBio>
 				<Zoomy href="http://projects.loratadin.com.s3-website-us-east-1.amazonaws.com/react-dog-app-tutorial/" target="_blank">
 					<img
-						src={require('../assets/proj-s-dogsperience.jpg')}
+						src={require('../assets/proj-s-dog-breed.jpg')}
 						alt={'dog breed app'}
 					/>
 				</Zoomy>
