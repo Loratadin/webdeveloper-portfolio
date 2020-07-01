4bfc0ed3208a7062b82707b1e4ac2d706a50fcc4
Tatsiana Shulha <taniaissohappy@gmail.com>
2020-07-01 10:48:23 -0500

    update video talk layout

--- src/components/VideoTalk/VideoTalk.js
+++ src/components/VideoTalk/VideoTalk.js
@@ -36,13 +36,17 @@ export default class VideoTalk extends Component {
 
     updateProgress = () => {
         const player = document.getElementById("player");
-        const percentage = Math.floor((100 / player.duration) * player.currentTime);
+        const percentage = player && Math.floor((100 / player.duration) * player.currentTime);
         this.setState({ progress: percentage });
     }
 
     switchFromGifToVideo = () => {
         this.setState({ isGifHidden: true });
-        this.toggleVideoPlay();
+        setTimeout(() => {
+            const player = document.getElementById("player");
+            player.play();
+            this.setState({ isVideoPlaying: true });
+          }, 1);
     }
     switchFromVideoToGif = () => {
         const player = document.getElementById("player");
@@ -57,34 +61,34 @@ export default class VideoTalk extends Component {
         return (
             <div className="video-talk__section">
                 <div className="video-talk__question">What does programming mean to you?</div>
-                <div className={`${isGifHidden ?  "video-talk__wrapper" : "video-talk__wrapper-hidden"}`}>
-                    <video onClick={this.toggleVideoPlay} id="player" className="video-talk__video" >
-                        <source type="video/mp4" src="https://s3.amazonaws.com/webdeveloper-portfolio-assets/about-me.mp4"/>
-                    </video>
-                    <div onClick={this.switchFromVideoToGif} className="video-talk__close-button">x</div>
-                    <div className="video-talk__controls">
-                        <div onClick={this.toggleVideoPlay} className={`${!isVideoPlaying || isVideoFinished ? "video-talk__control-wrap" : "video-talk__control-hidden"}`}>
-                            <img className={`${isVideoPlaying ? "video-talk__control-hidden" : "video-talk__control-img"}`} src={require('../../assets/play.png')} alt="play"/>
-                        </div>
-                        <div onClick={this.toggleVideoPlay} className={`${isVideoPlaying ? "video-talk__control-wrap" : "video-talk__control-hidden"}`}>
-                            <img className={`${isVideoPlaying ?  "video-talk__control-img" : "video-talk__control-hidden"}`} src={require('../../assets/pause.svg')} alt="pause"/>
-                        </div>
-                        <div  className="video-talk__progress">
-                            <div className="video-talk__progress-fill" style={{ width: progressFillWidth }}/>
+                <div className={`${isGifHidden ? "video-talk__video-grid-container" : "video-talk__gif-grid-container"}`}>
+                    <div className={`${isGifHidden ? "video-talk__video-container" : "video-talk__video-container-hidden"}`}>
+                        <video onClick={this.toggleVideoPlay} id="player" className={`${isGifHidden ? "video-talk__video" : "video-talk__video-hidden"}`}>
+                            <source type="video/mp4" src="https://s3.amazonaws.com/webdeveloper-portfolio-assets/about-me.mp4"/>
+                        </video>
+                        <div onClick={this.switchFromVideoToGif} className="video-talk__close-button">x</div>
+                        <div className="video-talk__controls">
+                            <div onClick={this.toggleVideoPlay} className={`${!isVideoPlaying || isVideoFinished ? "video-talk__control-wrap" : "video-talk__control-hidden"}`}>
+                                <img className={`${isVideoPlaying ? "video-talk__control-hidden" : "video-talk__control-img"}`} src={require('../../assets/play.png')} alt="play"/>
+                            </div>
+                            <div onClick={this.toggleVideoPlay} className={`${isVideoPlaying ? "video-talk__control-wrap" : "video-talk__control-hidden"}`}>
+                                <img className={`${isVideoPlaying ?  "video-talk__control-img" : "video-talk__control-hidden"}`} src={require('../../assets/pause.svg')} alt="pause"/>
+                            </div>
+                            <div  className="video-talk__progress">
+                                <div className="video-talk__progress-fill" style={{ width: progressFillWidth }}/>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className={`${isGifHidden ? "video-talk__grid-container-hidden" : "video-talk__grid-container"}`}>
-                    <div className="video-talk__quote-wrapper">
+                    <div className={`${isGifHidden ? "video-talk__gif-container-hidden" : "video-talk__gif-container"}`}>
+                        <img src={require('../../assets/about-me.gif')} alt="" className="video-talk__gif" />
+                            <div onClick={this.switchFromGifToVideo} className={`${isGifHidden ? "video-talk__gif-watch-hidden" : "video-talk__gif-watch"}`}>
+                                <div className="video-talk__gif-watch-content">WATCH</div>
+                                <img className="video-talk__gif-watch-icon" src={require('../../assets/play.png')} alt="play"/>
+                            </div>
+                    </div>
+                    <div className={`${isGifHidden ? "video-talk__quote-hidden" : "video-talk__quote-wrapper"}`}>
                         <div className="video-talk__quote">
                             Without hard work and discipline it is difficult to be a professional. You can’t cheat the grind, it knows how much you have invested, it won’t give you anything you haven’t worked for.
-                        </div>
-                    </div>
-                    <div className="video-talk__gif-wrapper">
-                        <img src={require('../../assets/about-me.gif')} alt="" className="video-talk__gif" />
-                        <div onClick={this.switchFromGifToVideo} className="video-talk__gif-watch">
-                            <div className="video-talk__gif-watch-content">WATCH</div>
-                            <img className="video-talk__gif-watch-icon" src={require('../../assets/play.png')} alt="play"/>
                         </div>
                     </div>
                 </div>
